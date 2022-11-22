7203e5135be76fd55d62ea5b56d4f4ad5d97e824
noak1 <kaonthana@gmail.com>
2022-11-21 21:01:43 -0500

    module3 fix

--- jobs/app.py
+++ jobs/app.py
@@ -20,6 +20,7 @@ def execute_sql(sql, values = (), commit = False, single = False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
     return results
 
 @app.teardown_appcontext
